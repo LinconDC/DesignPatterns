4a3f4b7f7c0627d8ac912bdbe18b9ae063daf97e
Lincon Martins <linconmartins18@gmail.com>
2022-07-13 21:29:55 -0300

    Design Patterns - Proxy

--- Loja/src/br/com/alura/loja/TestesComposicao.java
+++ Loja/src/br/com/alura/loja/TestesComposicao.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.orcamento.OrcamentoProxy;
 
 public class TestesComposicao {
 
@@ -11,13 +12,21 @@ public class TestesComposicao {
 
 		Orcamento antigo = new Orcamento();
 		antigo.adicionarItem(new ItemOrcamento(new BigDecimal("200")));
-		antigo.reprovar();
 		
 		Orcamento novo = new Orcamento();
 		novo.adicionarItem(new ItemOrcamento(new BigDecimal("500")));
 		novo.adicionarItem(antigo);
 		
-		System.out.println(novo.getValor());
+		OrcamentoProxy proxy = new OrcamentoProxy(novo);
+		
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
+		System.out.println(proxy.getValor());
+		
 	}
 
 }
